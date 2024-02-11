cfbd1044ab98828792539c99e15ad6404e162b86
Prakhar <prakhar.agarwal1205@gmail.com>
2024-02-11 21:48:12 +0530

    adding project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import homeImage from './home.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [clickCount, setClickCount] = useState(0);
+  const [yesFlag, setYesFlag] = useState(false);
+
+  var buttonSize = 100 + clickCount * 120;
+  console.log(buttonSize)
+  const increaseSize = () => {
+    // console.log("here");
+    setClickCount(clickCount + 1);
+  }
+
+  if(yesFlag) {
+
+    return (
+      <div>
+        <a href="/"><img id='home-img' src={homeImage} alt="Home" /></a>
+        <h1>I Love You!</h1>
+        <div className="center-container">
+          <img className="yes-gif" src='https://media1.tenor.com/m/fE_7w7DHRf8AAAAC/bears-hug.gif'></img>
+        </div>
+      </div>
+    );
+
+  } else {
+
+      return (
+        <>
+      <div>
+        <h1>
+          Will you be my valentine ?
+        </h1>
+
+        <div id="button-div">
+          <button id='yes-btn' type="button" className='btn btn-success mx-4' style={{fontSize:`${buttonSize}%`}} onClick = {() => setYesFlag(true)}>Yes</button>
+          <button id='no-btn' type="button" onClick = {() => increaseSize()} className="btn btn-danger mx-4">No</button>
+        </div>
+
+      </div>
+      </>
+    );
+  }
 }
 
 export default App;
